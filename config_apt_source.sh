6f84e4af8b936f3f262d417f6f57782dcf96eb16
songquanheng <songquanheng@zhijianglab.com>
2025-01-06 18:02:06 +0800

    [MOD]Configure ssh in the container by calling config_apt_source. sh in config_ssh. sh

--- config_apt_source.sh
+++ config_apt_source.sh
@@ -2,7 +2,8 @@
 
 cd $(dirname $0)
 # 定义源文件路径前缀
-source_type=$1
+source_type="ali"
+source_type="${1:-ali}"
 SOURCE_FILE_SUFFIX=".list"
 
 # 读取 /etc/os-release 文件
@@ -27,13 +28,22 @@ case $VERSION_ID in
         ;;
 esac
 
+if grep -q 'ali' "/etc/apt/sources.list" ; then
+    echo "系统已经配置了ali的apt源, 因此不需要配置apt源"
+    exit 0
+fi
+
+if grep -q 'qinghua' "/etc/apt/sources.list" ; then
+    echo "系统已经配置了qinghua的apt源, 因此不需要配置apt源"
+    exit 0
+fi
 
 # 根据用户的选择设置源文件名
 case $source_type in
-    1)
+    "qinghua")
         SOURCE_FILE="source/qinghua-ubuntu-$VERSION_ID.${SOURCE_FILE_SUFFIX}"
         ;;
-    2)
+    "ali")
         SOURCE_FILE="source/ali-ubuntu-$VERSION_ID.${SOURCE_FILE_SUFFIX}"
         ;;
     *)
@@ -44,7 +54,7 @@ esac
 
 # 检查并备份 sources.list
 if [ -f /etc/apt/sources.list ]; then
-    sudo cp /etc/apt/sources.list /etc/apt/sources.list.bak
+    cp /etc/apt/sources.list /etc/apt/sources.list.bak
     echo "sources.list has been backed up to sources.list.bak"
 else
     echo "No existing sources.list file found."
@@ -54,7 +64,7 @@ fi
 if [ -d /etc/apt/sources.d/ ]; then
     for file in /etc/apt/sources.d/*; do
         if [ -f "$file" ]; then
-            sudo cp "$file" "${file}.bak"
+            cp "$file" "${file}.bak"
             echo "Backed up $file to ${file}.bak"
         fi
     done
@@ -63,9 +73,9 @@ else
 fi
 
 # 拷贝新的源文件到 /etc/apt/
-sudo cp "$SOURCE_FILE" /etc/apt/sources.list
+cp "$SOURCE_FILE" /etc/apt/sources.list
 
 # 更新 apt 缓存
-sudo apt update
+apt update
 
 echo "APT source configuration completed."
