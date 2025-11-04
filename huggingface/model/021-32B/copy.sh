547f9b8e6ca2d46780629063966de061182f2fc7
songquanheng <songquanheng@zhejianglab.com>
2025-11-04 15:08:44 +0800

    save script

--- huggingface/model/021-32B/copy.sh
+++ huggingface/model/021-32B/copy.sh
@@ -5,3 +5,4 @@ TARGET_DIR="/mnt/seed-program-nas/001688/songquanheng/model/iter_0050000_hf_new"
 
 # 拷贝本目录下的modeling_deepseek.py到目标目录
 cp ./modeling_deepseek.py "$TARGET_DIR/"
+cp ./config.json "$TARGET_DIR/"
