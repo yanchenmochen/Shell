01060fe6a73c018d839c04b67c971200d2c3084e
songquanheng <songquanheng@zhejianglab.com>
2025-11-10 14:08:46 +0800

    [add] add description to copy.sh

--- huggingface/model/021-32B/copy.sh
+++ huggingface/model/021-32B/copy.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+# 脚本当前可运行于 镜像10.200.53.208/001688/021-32b-inference:v1
+# 脚本主要的目的，主要在修改modeling_deepseek.py 是将config.json、modeling_deepseek.py 拷贝到模型目录下，
+# ，然后调用python $shell_home/huggingface/model_generator_021.py 可以载入这个。
+# 在运行时，会将modeling_deepseek.py 拷贝到cache目录下 ~/.cache/huggingface/modules/transformers_modules
+# 想要断点的话，可以打开该文件添加，实现断点调试的功能
 
 # 目标目录
 TARGET_DIR="/mnt/seed-program-nas/001688/songquanheng/model/iter_0050000_hf_new"
